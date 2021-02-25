1910c4d7525522e4c29134e111cefd227e192f8e
Hiten Bahri <hiten.bahri@gmail.com>
2021-02-25 15:31:24 +1100

    Refactoring

--- app/src/main/java/com/example/jetpack/JetpackActivity.kt
+++ app/src/main/java/com/example/jetpack/JetpackActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpack
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
@@ -33,7 +34,7 @@ class JetpackActivity: AppCompatActivity() {
         MaterialTheme {
             val typo = MaterialTheme.typography
             Row(modifier = Modifier.padding(top = 8.dp,
-                    bottom = 4.dp)) {
+                    bottom = 4.dp).background(color = Color.White)) {
                 Image(
                         imageResource(id = drawable),
                         modifier = Modifier
@@ -54,14 +55,18 @@ class JetpackActivity: AppCompatActivity() {
 
     @Composable
     @Preview
-    fun MyPuppiesContent(names: List<String> = listOf("Akita", "Alaskan Malamute","Beagle", "Bichon Frise", "Norwich Terrier"),
-                         lifespan: List<String> = listOf("8 years", "10 years", "13 years", "16 years", "15 years"),
+    fun MyPuppiesContent(names: List<String> = listOf("Akita", "Alaskan Malamute","Beagle", "Bichon Frise", "Jack Russel", "Norwich Terrier", "Pomeranian", "Portuguese Water Dog"),
+                         lifespan: List<String> = listOf("8 years", "10 years", "13 years", "16 years","14 years", "15 years", "10 TO 12 years", "14 years"),
                          compatibility: List<String> = listOf("Tends to show dominance over other dogs.",
                                  "The breed is known for showing aggression towards other dogs.",
                                  "Beagles are pack hounds and as such are definitely better off with some company during the day.",
                                  "The Bichon Frise adapts well to other pets if reared with them, or introduced to them gradually. It will cope with both cats and other dogs quite well.",
-                                 "Norwich Terriers are very adaptable little dogs and will get on well with other family pets."),
-                         drawable: List<Int> = listOf(R.drawable.akita, R.drawable.alaskan, R.drawable.beagle, R.drawable.bichon, R.drawable.norwich)) {
+                                 "It is pretty Excellent and is a very friendly dog..",
+                                 "Norwich Terriers are very adaptable little dogs and will get on well with other family pets.",
+                                 "The Pomeranian has a friendly disposition but as with any new family pet, should be introduced carefully to any other resident companion animals.",
+                                 "When brought up with other pets in the home, they will form a strong friendship and respect for each other. They generally get on very well with other dogs when outside their home environment."
+                             ),
+                         drawable: List<Int> = listOf(R.drawable.akita, R.drawable.alaskan, R.drawable.beagle, R.drawable.bichon, R.drawable.jack, R.drawable.norwich, R.drawable.pomeranian, R.drawable.portuguese)) {
 
 
         Column (
@@ -69,7 +74,7 @@ class JetpackActivity: AppCompatActivity() {
                         .padding(8.dp)
                         .background(Color.Transparent)
         ) {
-            Column(modifier = Modifier.weight(1f)) {
+            ScrollableColumn(modifier = Modifier.weight(1f)) {
                 for((i, name) in names.withIndex()) {
                     getRow(name = name, lifespan = lifespan[i], compatibility[i], drawable[i])
                     Divider(color = Color.Black)
